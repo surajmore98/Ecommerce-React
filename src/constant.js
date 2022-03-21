ec101292525b8cff9f17e20dfeda89939e54436b
suraj more <29055479+surajmore98@users.noreply.github.com>
2022-03-21 23:26:55 +0530

    #fix - constant value changed.

--- src/constant.js
+++ src/constant.js
@@ -1,16 +1,16 @@
-export const sortByPrice = "Price";
-export const highToLow = "High To Low";
-export const lowToHigh = "Low To High";
+export const sortByPrice = "PRICE";
+export const highToLow = "HIGH_TO_LOW";
+export const lowToHigh = "LOW_TO_HIGH";
 
-export const filterByGender = "Gender";
-export const men = "Men";
-export const women = "Women";
+export const filterByGender = "FILTER_BY_GNEDER";
+export const men = "MEN";
+export const women = "WOMEN";
 
-export const filterByBrand = "Sort By Brand";
-export const adidas = "Adidas";
-export const nike = "Nike";
-export const puma = "Puma";
-export const reebok = "Reebok";
+export const filterByBrand = "FILTER_BY_BRAND";
+export const adidas = "ADIDAS";
+export const nike = "NIKE";
+export const puma = "PUMA";
+export const reebok = "REEBOK";
 
-export const filterByRating = "Rating";
-export const clearFilter = "Clear Filter";
+export const filterByRating = "FILTER_BY_RATING";
+export const clearFilter = "CLEAR_FILTER";
